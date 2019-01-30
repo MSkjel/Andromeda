19877435fd57ea1b9b85add6e3031263b6e69079
MSkjel <fuckbio@hotmail.com>
2019-01-30 20:33:23 +0100

    Reorganize

--- Andromeda/DSR.cs
+++ Andromeda/DSR.cs
@@ -40,30 +40,13 @@ namespace Andromeda
                 yield return BaseScript.Wait(0.5f);
 
                 while (LoadedDSR == "")
-                {
                     yield return BaseScript.Wait(0.05f);
-                }
 
                 ReadDSROptions(LoadedDSR);
                 Events.Events.FinishedLoadingDSROptions.Run(null, new Events.EventArguments.FinishedLoadingDSROptionsArgs(DSROptions));
             }
 
             Async.Start(routine());
-        }
-
-        public static void SetNextMapRotation(string map, string dsr)
-        {
-            if (DSRExists(dsr))
-            {            
-                File.WriteAllText($"{DSRFolder}\\{DSPL}.dspl", $"{map},{dsr},1");
-                GSCFunctions.SetDevDvarIfUninitialized("sv_defaultmaprotation", GSCFunctions.GetDvar("sv_maprotation"));
-                GSCFunctions.SetDvar("sv_maprotation", DSPL);
-                NextMapRotation = $"{map},{ dsr}";
-
-                return;
-            }
-
-            throw new ArgumentException($"DSR: {dsr} does not exist");
         }
 
         private static void ReadDSROptions(string dsrName)
@@ -91,6 +74,21 @@ namespace Andromeda
             {
                 Log.Error(e);
             }
+        }
+
+        public static void SetNextMapRotation(string map, string dsr)
+        {
+            if (DSRExists(dsr))
+            {
+                File.WriteAllText($"{DSRFolder}\\{DSPL}.dspl", $"{map},{dsr},1");
+                GSCFunctions.SetDevDvarIfUninitialized("sv_defaultmaprotation", GSCFunctions.GetDvar("sv_maprotation"));
+                GSCFunctions.SetDvar("sv_maprotation", DSPL);
+                NextMapRotation = $"{map},{ dsr}";
+
+                return;
+            }
+
+            throw new ArgumentException($"DSR: {dsr} does not exist");
         }
 
         public static void SetNextMap(string map) => SetNextMapRotation(map, LoadedDSR);
